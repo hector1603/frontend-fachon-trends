b968c5dbe99eca5ac84be40267cf278295d67aed
Hector Ajumado <hector.ajumado@gmail.com>
2025-08-16 03:32:44 -0400

    Sistema sin estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InventoryComponent } from './components/inventory/inventory';
 import { CompanyInfo } from './data/inventory.data';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    InventoryComponent
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
-  title = 'Sistema de Inventario para "Fashion Trends"'; [cite: 1]
+  title = 'Sistema de Inventario para "Fashion Trends"';
   companyInfo = CompanyInfo;
 }
